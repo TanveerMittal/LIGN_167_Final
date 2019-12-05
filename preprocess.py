659aeb6a25faa11e91e98a36910dbf5999ccaaf3
Tanveer <tamittal@ucsd.edu>
2019-12-04 20:10:25 -0800

    Started implemenation of LSTM with keras

--- preprocess.py
+++ preprocess.py
@@ -39,7 +39,7 @@ def json_to_dctv2(raw):
 pickle.dump(json_to_dctv1(json.load(open("data/train-v1.1.json"))), open("data/train-v1.1.pkl", "wb"))
 pickle.dump(json_to_dctv1(json.load(open("data/dev-v1.1.json"))), open("data/dev-v1.1.pkl", "wb"))
 
-def refactor_embeddings(words_to_index, index_to_words, word_to_vec_map):
+def torch_embeddings(words_to_index, index_to_words, word_to_vec_map):
     dim = len(word_to_vec_map['a'])
     for key in word_to_vec_map.keys():
         word_to_vec_map[key] = torch.from_numpy(word_to_vec_map[key].reshape((dim, 1))).type(torch.float32)
@@ -70,8 +70,39 @@ def refactor_embeddings(words_to_index, index_to_words, word_to_vec_map):
 
     return words_to_index, index_to_words, word_to_vec_map
 
+def np_embeddings(words_to_index, index_to_words, word_to_vec_map):
+    dim = len(word_to_vec_map['a'])
 
-embeddings = "glove.6B.100d.txt"
+    def vocab_append(word, embedding, index=None):
+        if index is None:
+            index = len(index_to_words)
+        word_to_vec_map[word] = embedding
+        index_to_words[index] = word
+        words_to_index[word] = index
+
+    # Unknown token embedding representation
+    unk = np.zeros((dim))
+    for emb in word_to_vec_map.values():
+        unk += emb
+    unk = unk / len(word_to_vec_map)
+    vocab_append("<unk>", unk, index=0)
+
+    # Seed numpy for random embedding generation
+    np.random.seed(50)
+
+    # Seperation of query and context token
+    vocab_append("<sep>", np.random.rand(dim))
+    # Start of answer token
+    vocab_append("<start>", np.random.rand(dim))
+    # End of answer token
+    vocab_append("<end>", np.random.rand(dim))
+    # Padding token
+    vocab_append("<pad>", np.random.rand(dim))
+
+    return words_to_index, index_to_words, word_to_vec_map
+
+
+embeddings = "glove.6B.50d.txt"
 words_to_index, index_to_words, word_to_vec_map = read_glove_vecs("embeddings/" + embeddings)
-words_to_index, index_to_words, word_to_vec_map = refactor_embeddings(words_to_index, index_to_words, word_to_vec_map)
+words_to_index, index_to_words, word_to_vec_map = np_embeddings(words_to_index, index_to_words, word_to_vec_map)
 pickle.dump((words_to_index, index_to_words, word_to_vec_map), open("embeddings/%s.pkl" % embeddings[:-4], 'wb'))
